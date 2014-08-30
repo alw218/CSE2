a3fee26eb1755ae0ec9ef29cf8591dfa1890bbff
alw218 <alw218@lehigh.edu>
2014-08-30 18:16:42 +0000

    Created WelcomeClass/hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+// Amber Wallace
+// hw01
+// Welcome Class Java Program
+// 8/30/2014
+
+// define class
+public class WelcomeClass{
+    //add main method
+    public static void main(String[] args){
+        //print code
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-A--L--W--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("Growing up in Maine, I've learned to love skiing.");
+        
+    }
+}
