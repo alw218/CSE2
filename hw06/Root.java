9b0e796872e6cf90a07dd79dc0068ff34fcf2670
alw218 <alw218@lehigh.edu>
2014-10-15 01:29:34 +0000

    Root finished

--- hw06/Root.java
+++ hw06/Root.java
@@ -9,20 +9,6 @@ and then prints it out. Use 0.0000001 as the “tolerance” for stopping the
 loop that computes the square root. 
 */
 
-/*
-Start by setting low to 0 and high to 1+x.
-Now take the middle of the interval [low, high] and call it middle. 
-If the square of middle is greater than x, then change high to middle; 
-otherwise change low to middle. At this point the square root is still between 
-low and high, but the distance between low and high has been halved. 
-Repeat this process until the difference between high and low is less than 
-some small multiple of 1+x, say 0.0000001*(1+x). Then the square root is very 
-close to both low and high. For example, to find the square root of 2, start 
-with [0,3] (0*0<2 and 3*3 > 2). The middle is 1.5. 1.5*1.5=2.25>2, so the 
-new interval is [0,2.25]. Now the middle is 1.125. 1.125*1.125<2, so the new 
-interval is [1.125,2.25]. And so on.
-*/
-
 //import scanner
 import java.util.Scanner;
 //create class
@@ -32,28 +18,34 @@ public class Root{
         //create new scanner
         Scanner input=new Scanner(System.in);
         //get input
-        double x=-5;
+        double x;
         System.out.print("Enter a double: ");
-        if(input.hasNextDouble()){
-            x=input.nextDouble();
+        if(input.hasNextDouble()){ //check for double
+            x=input.nextDouble(); //accept double
         }
         else{
             //reject input
             System.out.println("That is not a double.");
             return;
         }
-        double low=0;
-        double high=x+1;
-        double middle=(low+high)/2;
-        while(high-low>0.0000001*(x+1)){
-            middle=(low+high)/2;
-            if(middle*middle>x){
-                high=middle;
+        if(x<0){ 
+            //reject input
+            System.out.println("The double must be positive.");
+            return;
+        }
+        double low=0; //set initial low value
+        double high=x+1; //set initial high value
+        double middle=(low+high)/2; //set initial middle value
+        while(high-low>0.0000001*(x+1)){ //test if guess for root is close enough
+            middle=(low+high)/2; //update guess for middle
+            if(middle*middle>x){ 
+                high=middle; //set high as middle value
             }
             else{
-                low=middle;
+                low=middle; //set low as middle value
             }
         }
+        //print results
         System.out.println("The square root of "+x+" is "+middle);
     }
 }
