12376e395a0206768143068ea89f630fc9713ad9
alw218 <alw218@lehigh.edu>
2014-12-05 02:31:26 +0000

    did error report for Enigma2

--- lab06/Enigma2.java
+++ lab06/Enigma2.java
@@ -1,3 +1,7 @@
+//Amber Wallace
+//10/5/2014
+//lab06-Enigma2
+
 import java.util.Scanner;
 public class Enigma2{
   public static void main(String [] arg){
@@ -6,7 +10,7 @@ public class Enigma2{
     switch(n){
       case 12: 
       case 13: out+=13;
-      case 40: out+=40/0;
+      case 40: out+=40; //no division by zero
     }
     System.out.println(out);
   }
@@ -15,7 +19,7 @@ public class Enigma2{
 /*
  * Error Report: Explain how you can use the java.lang exception to
  *     identify the runtime error in the above code
- * 
+ *  division by zero is not acceptable
  * 
  * 
  * 
