a9dbc301fd0fc5ad613f311477cb724bdd4c9315
alw218 <alw218@lehigh.edu>
2014-12-05 02:26:38 +0000

    finished enigma0

--- lab06/Enigma0.java
+++ lab06/Enigma0.java
@@ -14,21 +14,21 @@ public class Enigma0{
     public static void main(String arg []){
         System.out.print("Enter an int- ");
         Scanner scan=new Scanner(System.in);
-        int n; //variable must be declared here for proper scope
+        String n; //variable must be declared here for proper scope also must be string
         if(scan.hasNextInt()){
-            n=scan.nextInt();
+            n=scan.next();
             System.out.println("You entered "+n);
         }
         else{
-            n=4;
-            System.out.println("You entered "+n);
+            n=scan.next(); //don't auto set as 4, store acutal input
+            System.out.println("You did not enter an int."); //report issue
         }
     
         int k=4,p=6,q=7,r=8;
         switch(k+p+q+r){
-            case 24: 
+            case 24: System.out.println("sum is 24"); //the sum would be 24 not 25
+                     break; //need break so 24 and 25 aren't both printed
             case 25: System.out.println("sum is 25");
-            default:
                 System.out.println("Again, you entered "+n);
         }
     }
@@ -38,5 +38,7 @@ public class Enigma0{
  *   Scope error with int n;
  *   Had to move the declaration of int n outside of 
  *   and above the if statement.
+ *   Had to change int n to string n so it could be called instead of autosetting n=4
+ *   add different value for sum is 24 and add break
  */
 
